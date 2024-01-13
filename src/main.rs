9fb44f22e2f43492637bf66158e2b964be7971e8
Tom Brzyzek <tbrzyzek@gmail.com>
2024-01-13 16:15:59 -0500

    20240112 - Button to enable LED. No debouncing.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-//! Demonstrate the use of a blocking `Delay` using the SYST (sysclock) timer.
-
 #![deny(unsafe_code)]
 #![allow(clippy::empty_loop)]
 #![no_main]
 #![no_std]
 
+use hal::gpio::Pin;
 // Halt on panic
-use panic_halt as _; // panic handler
+use panic_halt as _;
 
 use cortex_m_rt::entry;
 use stm32f4xx_hal as hal;
@@ -19,21 +18,18 @@ fn main() -> ! {
         pac::Peripherals::take(),
         cortex_m::peripheral::Peripherals::take(),
     ) {
-        // Set up the LED. On the Nucleo-446RE it's connected to pin PA5.
         let gpioa = dp.GPIOA.split();
+        let gpioc = dp.GPIOC.split();
         let mut led = gpioa.pa5.into_push_pull_output();
+        let button = gpioc.pc13.into_pull_up_input();
 
-        // Set up the system clock. We want to run at 48MHz for this one.
         let rcc = dp.RCC.constrain();
         let clocks = rcc.cfgr.sysclk(48.MHz()).freeze();
 
-        // Create a delay abstraction based on SysTick
-        let mut delay = cp.SYST.delay(&clocks);
-
         loop {
-            // On for 1s, off for 1s.
-            led.toggle();
-            delay.delay_ms(1000_u32);
+            if button.is_low(){
+                led.toggle();
+            }
         }
     }
 
